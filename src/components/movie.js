49ab54cce75b32acf61b8cba140a656250c3eaf2
donavonhayes <131027014+donavonhayes@users.noreply.github.com>
2023-07-06 12:26:18 -0500

    8 commit

--- src/components/movie.js
+++ src/components/movie.js
@@ -1,36 +1,16 @@
+import './App.css';
 import React from 'react';
-import ReviewForm from './reviewForm';
-import Review from './review';
+import MovieList from './components/movie-list';
+import Header from './components/header'
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
-class Movie extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            movieTitle: props.movieTitle,
-            reviews: [],
-        }
-
-        this.updateReviewList = this.updateReviewList.bind(this);
-    }
-
-    updateReviewList(review) {
-        let tmpReviews = this.state.reviews;
-        tmpReviews.push(review);
-        this.setState({reviews: tmpReviews});
-    }
-
-    render() {
-        let reviewComponents = this.state.reviews.map((review, key) => 
-        <Review key={key} reviewContent={review.reviewContent} stars={review.stars}/>);
-
-        return (
-            <div>
-                <h1>{this.state.movieTitle}</h1>
-                {reviewComponents}
-                <ReviewForm onClick={this.updateReviewList} movie={this.state.movieTitle} key={'review-form-' + this.state.movieTitle}/>
-            </div>
-        );
-    }
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MovieList />
+    </div>
+  );
 }
 
-export default Movie;
+export default App;
